971de29b6b21417b7797b837e9f1481503cb1e7f
chetan <chetanofficial048@gmail.com>
2025-12-19 19:19:46 +0530

    Initial website upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openWhatsApp() {
+  const msg = "Hello, I want to know about premium clothing at Reshmi Cloth Store.";
+  window.open(
+    "https://wa.me/919901095751?text=" + encodeURIComponent(msg),
+    "_blank"
+  );
+}
