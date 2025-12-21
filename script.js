cbabc67c3158c95d4dff81bc6e48fcd180f63978
chetan <chetanofficial048@gmail.com>
2025-12-21 20:05:07 +0530

    Remove 'premium' from WhatsApp message

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function openWhatsApp() {
-  const msg = "Hello, I want to know about premium clothing at Reshmi Cloth Store.";
+  const msg = "Hello, I want to know about  clothing at Reshmi Cloth Store.";
   window.open(
     "https://wa.me/919901095751?text=" + encodeURIComponent(msg),
     "_blank"
   );
 }
+
